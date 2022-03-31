7e3e09fa5a9b076f9bb537b3077e9abb4f077905
Allen Guan <me@nella17.tw>
2022-03-31 17:30:16 +0800

    set_no_buffer_mode for STDIN

--- README.md
+++ README.md
@@ -7,3 +7,4 @@
     - https://github.com/mbcrawfo/GenericMakefile/blob/master/cpp/Makefile
     - https://github.com/Cheedoong/MakefileTemplate/blob/master/Makefile
     - https://github.com/mattearly/best_makefile_for_cpp/blob/master/Makefile
+- https://stackoverflow.com/questions/31963835/how-to-read-terminals-input-buffer-immediately-after-keypress
--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,6 @@
+#include <fcntl.h>
+#include <termios.h>
+#include <unistd.h>
 #include <iostream>
 #include <string>
 #include <vector>
@@ -10,7 +13,16 @@
 
 using namespace std;
 
+void set_no_buffer_mode(int fd) {
+    struct termios tty;
+    tcgetattr(fd, &tty);
+    tty.c_lflag &= ~(ICANON|ECHO);
+    tcsetattr(fd, TCSANOW, &tty);
+}
+
 int main(){
+    set_no_buffer_mode(STDIN_FILENO);
+
     /*Dungeon dungeon = Dungeon();
     dungeon.runDungeon();*/
     cout << "meow" << endl;
