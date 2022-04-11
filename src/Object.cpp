06c9d6435fac34349c215ab4d91d96be086e7c57
Allen Guan <me@nella17.tw>
2022-04-11 14:53:50 +0800

    Implement Object

--- src/Object.cpp
+++ src/Object.cpp
@@ -1,1 +1,8 @@
 #include "Object.hpp"
+
+Object::Object(string _name, string _tag): name(_name), tag(_tag) {}
+
+void Object::setName(string _name) { name = _name; }
+void Object::setTag(string _tag) { tag = _tag; }
+string Object::getName() { return name; }
+string Object::getTag() { return tag; }
