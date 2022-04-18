b37a90c02120b71a8fe96196e4033f1b55ef73ba
Allen Guan <me@nella17.tw>
2022-04-18 14:15:29 +0800

    Implement Item

--- src/Item.cpp
+++ src/Item.cpp
@@ -3,7 +3,17 @@
 Item::Item(string _name, int _health, int _attack, int _defense):
         Object(_name, "Item"), health(_health), attack(_attack), defense(_defense) {}
 
-// TODO
-bool Item::triggerEvent(const Object_ptr) {
-    return true;
+bool Item::triggerEvent(const Object_ptr obj) {
+    if (obj->getTag() == "Player") {
+        shared_ptr<Player> player = dynamic_pointer_cast<Player>(obj);
+        return player->triggerEvent(shared_from_this());
+    }
+    return false;
 }
+
+int Item::getHealth() { return health; }
+int Item::getAttack() { return attack; }
+int Item::getDefense() { return defense; }
+void Item::setHealth(int _health) { health = _health; }
+void Item::setAttack(int _attack) { attack = _attack; }
+void Item::setDefense(int _defense) { defense = _defense; }
