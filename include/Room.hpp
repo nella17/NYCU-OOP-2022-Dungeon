4fc223ab3f543c76fc362c5d25727112066c8b45
Allen Guan <me@nella17.tw>
2022-04-19 11:39:32 +0800

    use inline function instead of define

--- include/Room.hpp
+++ include/Room.hpp
@@ -6,10 +6,6 @@
 #include "helper.hpp"
 #include "Object.hpp"
 
-#define LINK_ROOM(r1, d1, r2, d2) \
-    (r1)->setRoom(d2, r2); \
-    (r2)->setRoom(d1, r1);
-
 class Room {
 public:
     Room(bool = false, int = -1, Objects_map= {});
@@ -38,5 +34,10 @@ private:
     std::map<DIRECTION,Room*> neighborRooms;
     Objects_map objects;/*contain 1 or multiple objects, including monster, npc, etc*/
 };
+
+inline void LINK_ROOM(Room* r1, DIRECTION d1, Room* r2, DIRECTION d2) {
+    r1->setRoom(d2, r2);
+    r2->setRoom(d1, r1);
+}
 
 #endif // ROOM_H_INCLUDED
