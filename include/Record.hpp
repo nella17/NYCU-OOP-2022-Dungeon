e48e98ba5a6e143dda9ea563a0f3a1214f1c09bd
Allen Guan <me@nella17.tw>
2022-04-19 21:24:40 +0800

    Record has static member function

--- include/Record.hpp
+++ include/Record.hpp
@@ -14,13 +14,13 @@ class Room;
 
 class Record {
 public:
-    void save_to_file(const std::shared_ptr<Player>, const std::vector<Room>&);
-    void load_from_file(std::shared_ptr<Player>&, std::vector<Room>&);
+    static void save_to_file(const std::shared_ptr<Player>, const std::vector<Room>&);
+    static void load_from_file(std::shared_ptr<Player>&, std::vector<Room>&);
 private:
-    void save_player(const std::shared_ptr<Player>, std::ofstream&&);
-    void load_player(std::shared_ptr<Player>&, std::ifstream&&);
-    void save_rooms(const std::vector<Room>&, std::ofstream&&);
-    void load_rooms(std::vector<Room>&, std::ifstream&&);
+    static void save_player(const std::shared_ptr<Player>, std::ofstream&&);
+    static void load_player(std::shared_ptr<Player>&, std::ifstream&&);
+    static void save_rooms(const std::vector<Room>&, std::ofstream&&);
+    static void load_rooms(std::vector<Room>&, std::ifstream&&);
 };
 
 #endif // RECORD_H_INCLUDED
