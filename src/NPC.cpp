b9e593d6633e61affe10c45fd84937734530efaa
Allen Guan <me@nella17.tw>
2022-04-20 18:10:35 +0800

    Implement NPC

--- src/NPC.cpp
+++ src/NPC.cpp
@@ -11,9 +11,15 @@ NPC::NPC(std::string _name, std::string _script, ItemsSet _inventory):
         GameCharacter(_name, Object::Type::NPC),
         script(_script), inventory(std::make_shared<Inventory>(_name, _inventory)) {}
 
-void NPC::print_status(InteractablePtr) {}
-void NPC::print_menu() {}
-bool NPC::handle_key(int, ObjectPtr) { return false; }
+void NPC::print_status(InteractablePtr) {
+    std::cout << script << std::endl;
+}
+void NPC::print_menu() {
+    inventory->print_menu();
+}
+bool NPC::handle_key(int key, ObjectPtr obj) {
+    return inventory->handle_key(key, obj);
+}
 
 // TODO
 bool NPC::trigger_event(ObjectPtr) {
