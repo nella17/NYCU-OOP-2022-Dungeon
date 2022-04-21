aaa20bba573d9391304d63705afe3d27dfaebcce
Allen Guan <me@nella17.tw>
2022-04-21 09:22:34 +0800

    Implement Monster

--- src/Monster.cpp
+++ src/Monster.cpp
@@ -10,6 +10,27 @@
 Monster::Monster(std::string _name, int _maxHealth, int _attack, int _defense):
         GameCharacter(_name, Object::Type::Monster, _maxHealth, _attack, _defense) {}
 
-void Monster::print_status(InteractablePtr) {}
-void Monster::print_menu() {}
-bool Monster::handle_key(int, ObjectPtr) { return false; }
+void Monster::print_status(InteractablePtr) {
+    GameCharacter::print_status();
+}
+
+void Monster::print_menu() {
+    std::cout << "------ Monster Menu ---------\n";
+    std::cout << "  [A] \tAttack\n";
+}
+
+bool Monster::handle_key(int key, ObjectPtr obj) {
+    auto player = std::dynamic_pointer_cast<Player>(obj);
+    if (player == nullptr)
+        return false;
+
+    switch (key) {
+        case 'A': {
+            take_damage(player->get_attack(), player);
+            player->take_damage(get_attack(), shared_from_this());
+            return check_is_dead();
+        }
+        default:
+            return false;
+    }
+}
